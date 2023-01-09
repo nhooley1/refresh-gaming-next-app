460726cc7e1d0d346306b479b2308939225783fb
Nicholas Hooley <nicholashooley27@yahoo.co.uk>
2023-01-09 14:15:55 +0000

    stripe integration with test api calls

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,10 +1,10 @@
-// import { getProduct } from '../api/getProduct';
-// import { getProducts } from '../api/getProducts';
-
 import { getProducts } from '../api/products';
 import { getProduct } from '../api/products';
-
+import { useState } from 'react';
+import Head from 'next/head';
+import { loadStripe } from '@stripe/stripe-js';
 import Product from '../../components/Product';
+import { useEffect } from 'react';
 
 export async function getStaticPaths() {
   // Fetch the list of all products
@@ -33,7 +33,62 @@ export async function getStaticProps({ params }) {
   };
 }
 
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+);
+
 export default function ProductPage({ product }) {
-  console.log(product);
-  return <Product {...product.fields} />;
+  useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search);
+    if (query.get('success')) {
+      console.log('Order placed! You will receive an email confirmation.');
+    }
+
+    if (query.get('canceled')) {
+      console.log(
+        'Order canceled -- continue to shop around and checkout when you’re ready.'
+      );
+    }
+  }, []);
+
+  return (
+    <>
+      <Product {...product.fields} />
+      <form action="/api/checkout_sessions" method="POST">
+        <section>
+          <button type="submit" role="link">
+            Checkout
+          </button>
+        </section>
+        <style jsx>
+          {`
+            section {
+              background: #ffffff;
+              display: flex;
+              flex-direction: column;
+              width: 400px;
+              height: 112px;
+              border-radius: 6px;
+              justify-content: space-between;
+            }
+            button {
+              height: 36px;
+              background: #556cd6;
+              border-radius: 4px;
+              color: white;
+              border: 0;
+              font-weight: 600;
+              cursor: pointer;
+              transition: all 0.2s ease;
+              box-shadow: 0px 4px 5.5px 0px rgba(0, 0, 0, 0.07);
+            }
+            button:hover {
+              opacity: 0.8;
+            }
+          `}
+        </style>
+      </form>
+    </>
+  );
 }
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import Link from 'next/link';
-import { getProducts } from '../api/getProducts';
+import { getProducts } from '../api/products';
 import Product from '../../components/Product';
-import Card from '../../components/layout/Card';
 import styles from './index.module.css';
 
 function AllProductsPage({ products }) {
+  console.log(products);
+
+  let url = '';
+
+  products.map((product) => {
+    console.log(product.fields.title);
+    console.log(product.fields.description);
+    console.log(product.fields.id);
+    console.log(product.fields.inStock);
+    console.log(product.fields.price);
+    console.log(product.fields.title);
+    product.fields.image.map((image) => {
+      console.log(image.fields.file.url);
+      url = image.fields.file.url;
+    });
+  });
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>All Products</h1>
@@ -13,12 +29,12 @@ function AllProductsPage({ products }) {
         {products.map((product) => (
           <Link
             href="/products/[id]"
-            as={`/products/${product.id}`}
-            key={product.id}
+            as={`/products/${product.fields.id}`}
+            key={product.fields.id}
           >
-            <Card>
-              <Product {...product} />
-            </Card>
+            <div className={styles.product}>
+              <Product {...product.fields} />
+            </div>
           </Link>
         ))}
       </div>
@@ -27,7 +43,8 @@ function AllProductsPage({ products }) {
 }
 
 export async function getStaticProps() {
-  const products = getProducts();
+  // Fetch the products from the api
+  const products = await getProducts();
 
   return {
     props: {
